63069d469b787446c9af413d9f078b292d5fff15
juansetab <juanmanuel.trinidad@correo.setab.gob.mx>
2026-04-02 13:13:22 -0600

    Cambio de formato de constancias

--- app/Libraries/Documentos.php
+++ app/Libraries/Documentos.php
@@ -18,26 +18,35 @@ class Documentos
         $y = date("Y", strtotime($constancia["fecha"]));
         $pdf->SetTitle("Constancia G-$y{$constancia['folio']}");
         $pdf->AddPage();
-        $pdf->Image(base_url("public/images/constancia_base_pdf_2348659823759237523456.jpg"), 0, 0, 279, 215.5, 'JPG');
+        $pdf->Image(base_url("public/images/constancia_base_pdf_2348659823759237523456349567234.jpg"), 0, 0, 279, 215.5, 'JPG');
         $filename = WRITEPATH . "/uploads/temp/" . $constancia["qr"] . ".png";
-        $pdf->Image($filename, 210, 140, 55, 55, "png");
-
+        $pdf->Image($filename, 210, 140, 45, 45, "png");
         $pdf->SetFont('Arial', 'B', 18);
-        $pdf->SetXY(210, 35);
-        $pdf->Cell(50, 7, $this->UFT8(mb_strtoupper("FOLIO", "UTF-8")), 0, 1, "C");
-        $pdf->SetFont('Arial', 'B', 25);
-        $pdf->SetXY(210, 42);
+        $pdf->SetXY(28, 57.6);
         $pdf->SetTextColor(255, 0, 0);
-        $pdf->Cell(50, 8, $this->UFT8(mb_strtoupper($constancia['folio'], "UTF-8")), 0, 1, "C");
+        $pdf->Cell(30, 8, $this->UFT8(mb_strtoupper($constancia['folio'], "UTF-8")), 0, 1, "R");
         $pdf->SetTextColor(0, 0, 0);
-        $pdf->SetFont('Arial', 'B', 25);
-        $pdf->SetXY(37, 103);
+        $pdf->SetFont('Arial', 'B', 22);
+        $pdf->SetXY(37, 98);
         $pdf->Cell(212, 12, $this->UFT8(mb_strtoupper($constancia["nombre_alumno"], "UTF-8")), 0, 1, "C");
-        $pdf->SetFont('Arial', 'B', 13);
-        $pdf->SetXY(15, 196);
-        $pdf->Cell(212, 12, $this->UFT8("Framboyán 116, Col. Jesús García", "UTF-8"), 0, 1, "L");
-        $pdf->SetXY(100, 196);
-        $pdf->Cell(212, 12, $this->UFT8("993 322 6612", "UTF-8"), 0, 1, "L");
+        $pdf->SetFont('Arial', 'B', 20);
+        $pdf->SetXY(10, 135);
+        $fechaInicio = \App\Libraries\Utilidades::date_to_text($constancia["fecha_inicio"], false, "d");
+        $fechaFinal = \App\Libraries\Utilidades::date_to_text($constancia["fecha_final"], false, "d");
+        $periodoTxt = "";
+        $fechaInicio["m"] = ucfirst($fechaInicio["m"]);
+        $fechaFinal["m"] = ucfirst($fechaFinal["m"]);
+        if($fechaInicio["y"] == $fechaFinal["y"]){
+            if($fechaInicio["m"] == $fechaFinal["m"]){
+                $periodoTxt = "Del " . $fechaInicio["d"] . " al " . $fechaFinal["d"] . " de " . $fechaInicio["m"] . " del " . $fechaInicio["y"];
+            }else{
+                $periodoTxt = "Del " . $fechaInicio["d"] . " de " . $fechaInicio["m"] . " al " . $fechaFinal["d"] . " de " . $fechaFinal["m"] . " del " . $fechaInicio["y"];
+            }
+        }else{
+            $periodoTxt = "Del " . $fechaInicio["d"] . " de " . $fechaInicio["m"] . " del " . $fechaInicio["y"] . " al " . $fechaFinal["d"] . " de " . $fechaFinal["m"] . " del " . $fechaFinal["y"];
+
+        }
+        $pdf->Cell(260, 5, $this->UFT8($periodoTxt, "UTF-8"), 0, 1, "C");
         $fecha = \App\Libraries\Utilidades::date_to_text($constancia["fecha"], false, "d");
         $pdf->SetFont('Arial', 'B', 15);
         $pdf->SetXY(140, 196);
